699725cb16b5fc76338a96258c7ec6a56ded32a6
CANDELY001 <candely1charifa@gmail.com>
2023-10-05 22:30:10 +0000

    Fixed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,48 +1,27 @@
 #!/usr/bin/env bash
-#Bash script that sets up your web servers for the deployment of web_static
+# set up nginx
 
-echo -e "\e[1;35m START CONFIGURATION\e[0m\n"
+apt update -y
+apt install -y nginx
 
-# Install Nginx
-sudo apt-get -y update
-sudo apt-get -y install nginx
-echo -e "\e[1;35m Nginx Installed\e[0m\n"
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
 
-# Allow incoming traffic on the default HTTP port (port 80)
-sudo ufw allow 'Nginx HTTP'
-echo -e "\e[1;35m Allow incoming traffic on the default HTTP port\e[0m\n"
+echo "<html>
+  <head></head>
+  <body>
+    Nginx test page!
+  </body>
+</html>" | tee /data/web_static/releases/test/index.html
 
-# Create directories
-sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
-echo -e "\e[1;35m Directories created\e[0m\n"
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Add a fake HTML file
-echo "<h1>Welcome to hajaralx.tech site</h1>" > /data/web_static/releases/test/index.html
-echo -e "\e[1;35m Simple content page added\e[0m\n"
+chown -R ubuntu:ubuntu /data/
 
-# Remove the symbole link if exists
-if [ -d /data/web_static/current ];
-then
-	echo "Remove the existing /data/web_static/current"
-	sudo rm -rf /data/web_static/current;
-fi;
-echo -e "\e[1;35m The existing of /data/web_static/current dir checked\e[0m\n"
+location="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
 
-# Create the symbolik link
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
-echo -e "\e[1;35m Symbolic link created\e[0m\n"
+sudo sed -i "/server_name _;/a $location" /etc/nginx/sites-available/default
 
-# Give ownership of the /data/ folder to the ubuntu user AND group
-sudo chown -hR ubuntu:ubuntu /data
-echo -e "\e[1;35m User & Group setted\e[0m\n"
-
-# Update nginx Configuration
-sudo sed -i '47i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
-echo -e "\e[1;35m Serve /data/web_static/current/ to hbnb_static\e[0m\n"
-
-# Restart nginx service
 sudo service nginx restart
-echo -e "\e[1;35m Nginx service restarted\e[0m\n"
 
-echo -e "\e[1;35m END CONFIGURATION\e[0m\n"
+exit 0
